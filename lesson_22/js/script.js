868e6fa8a7bee5d8515aa157d893878bce1105bf
Valerii <106310789+ValeriiKom@users.noreply.github.com>
2022-11-29 19:21:45 +0200

    Add files via upload

---
+++ lesson_22/js/script.js
@@ -0,0 +1,66 @@
+"use strict"
+
+console.log('Я вивчаю JS!');
+
+//Варіант №1
+console.log('Вивчаємо JS')
+
+//Варіант №2
+console.log('JS')
+console.log('Вивчаємо')
+
+//Варіант №3
+console.log('Вивчаємо'); console.log('JS');
+
+//Варіант №4
+console.log('Вивчаємо');
+console.log('JS');
+
+
+
+// let userEyeColor;
+// let user;
+// let userName;
+// userName = 'Вася';
+// user = userName;
+// console.log(user);
+// console.log(userName);
+
+//Варіант №1
+let user = 'Вася'; let age = 36;
+
+//Варіант №2
+let myAge = 36;
+
+//Варіант №3Ж
+const BLOCK_SIZE = 14 + 50;
+
+//Варіант №4
+const BLOCK_HEIGHT = 100;
+
+//Варіант №5
+if (true) {
+	var size = 15;
+}
+console.log(size);
+
+
+//Вариант №1
+let userAge = 36;
+let userInfo = `Фрилансер ${userAge}`;
+console.log(userInfo);
+
+//Вариант №2
+let userHeight = 145 / 0;
+//Вернется значение NaN
+console.log(userHeight);
+
+//Вариант №3
+let userName;
+//Вернется тип данных Null
+console.log(typeof userName);
+
+//Вариант №4
+let userSize = "45" / "8";
+//Вернется тип данных Number
+console.log(typeof userSize);
